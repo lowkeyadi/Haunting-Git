3be2fdc462d0717dfcacfbf15201700bb5627ad2
HilaZ04 <hila.zutta@gmail.com>
2026-04-15 21:07:15 +0300

    animation works!!!
    yay
    now all needed it to finish the whole interaction system

--- Assets/Code check/Code/interaction system/Compontents/PlayScareAnimation.cs
+++ Assets/Code check/Code/interaction system/Compontents/PlayScareAnimation.cs
@@ -3,15 +3,40 @@ using UnityEngine;
 public class PlayScareAnimation : MonoBehaviour, IInteractable
 {
     [SerializeField] private Animator MyRoatat;
-  public string DisplayName { get; }
-   public bool CanInteract;
-    public void Interact()
-    {
-      
-    }
+    [SerializeField] private string displayName = "Scare Object";
+    [SerializeField] private bool canInteract = true;
+    private bool isOn = false;
+
+    public string DisplayName => displayName;
+
     private void Awake()
     {
-        
-   
+        if (MyRoatat == null)
+            MyRoatat = GetComponent<Animator>();
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Interact();
+        }
+    }
+
+    public void Interact()
+    {
+        if (!canInteract || MyRoatat == null)
+            return;
+
+        isOn = !isOn;
+        MyRoatat.SetBool("Scare", isOn);
+    }
+
+    public bool CanInteract()
+    {
+        return canInteract;
+    }
+
+    public void OnFocusGained() { }
+    public void OnFocusLost() { }
 }
